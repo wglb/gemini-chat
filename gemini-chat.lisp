6798cde9f63c033d6a5d849893ae4eeeb4b0f402
wglb <wgl@park.montana.ciexinc.com>
2025-08-12 16:42:22 -0500

    Improved error message if api burps

--- gemini-chat.lisp
+++ gemini-chat.lisp
@@ -431,7 +431,7 @@
         (flush-all-log-streams)
         (values (append conv-hist (list u-turn m-turn)) t))
       (progn
-        (xlgt :answer-log "~&Error on ~a: ~a: ~a" turn-type
+        (xlgt :answer-log "~&Error on ~a:~%[~a]: ~a" turn-type
               parsed-json "No text generated or unexpected response structure.")
         (when *run-out-s* ; Log API errors that prevent text generation, as they are part of the answer stream
           (format *run-out-s* "~&Error on ~a: [~a] ~a~%" turn-type
