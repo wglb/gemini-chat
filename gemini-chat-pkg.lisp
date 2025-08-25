4207d32d330df1d2a9c97f788fc3988e0c1540b4
wglb <wgl@park.montana.ciexinc.com>
2025-08-25 15:39:55 -0500

    Minor additons.

--- gemini-chat-pkg.lisp
+++ gemini-chat-pkg.lisp
@@ -2,10 +2,19 @@
 ;;;; This file defines the GEMINI-CHAT package.
 
 (defpackage #:gemini-chat
-  (:use #:cl #:xlg-lib #:com.google.flag #+nil #:uiop  #:uiop/driver)
-  (:import-from #:drakma #:http-request #:drakma-error) 
-  (:import-from #:jsown #:to-json #:parse) 
+  (:use #:cl
+        #:com.google.flag
+        #:uiop/driver
+        #:split-sequence
+        #:xlg-lib)
+  (:import-from #:drakma #:http-request #:drakma-error)
+  (:import-from #:jsown #:to-json #:parse)
   (:import-from #:uiop #:getenv #:slurp-stream-string #:read-file-forms)
   (:export #:run-gemini-conversation
            #:make-gemini-api-request
-           #:run-chat))
+           #:run-chat
+           #:slime-chat
+           #:save-core
+           #:save-core-uncompressed
+           #:top
+		   #:run-chat))
