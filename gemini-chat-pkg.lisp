723c1fa2e2f86e3b47f9beb8926563c9d1fe6071
wglb <wgl@park.montana.ciexinc.com>
2025-12-15 09:52:47 -0600

    Remove parameter error

--- gemini-chat-pkg.lisp
+++ gemini-chat-pkg.lisp
@@ -1,19 +1,9 @@
-;;;; gemini-chat-pkg.lisp
-;;;; This file defines the GEMINI-CHAT package.
-;;;; gemini-chat-pkg.lisp
-;;;; This file defines the GEMINI-CHAT package.
-
 (defpackage #:gemini-chat
   (:use #:cl
         #:jsown
         #:cl-ppcre
         #:split-sequence
 		#:file-packer-lib
-		#:cljwt        ; JWT signing (Requires Ironclad)
-        #:ironclad     ; Cryptography library (for cljwt)
-        #:cl-base64
-		#:jose
-		#:cl-ssh-keys
         #:xlg-lib
         #:com.google.flag
         #:uiop/driver
@@ -21,36 +11,7 @@
   (:import-from #:drakma 
     #:http-request 
     #:drakma-error 
-    #:url-encode 
-    #:parameter-error)
-  (:import-from #:jsown #:to-json #:parse)
-  (:import-from #:uiop #:getenv #:slurp-stream-string #:read-file-forms)
-  (:export #:run-gemini-conversation
-           #:slime-chat
-           #:top
-           #:api-req))
-
-
-#+nil (defpackage #:gemini-chat
-  (:use #:cl
-		#:drakma
-        #:jsown
-        #:cl-ppcre
-        #:split-sequence
-		#:file-packer-lib
-		#:cljwt        ; JWT signing (Requires Ironclad)
-        #:ironclad     ; Cryptography library (for cljwt)
-        #:cl-base64
-		#:jose
-		#:cl-ssh-keys
-        #:xlg-lib
-        #:com.google.flag
-        #:uiop/driver
-        #:split-sequence
-		#:file-packer-lib
-        #:xlg-lib
-		#:gemini-chat-lib)
-  (:import-from #:drakma #:http-request #:drakma-error)
+    #:url-encode) ; <--- Removed #:parameter-error, which was causing the conflict
   (:import-from #:jsown #:to-json #:parse)
   (:import-from #:uiop #:getenv #:slurp-stream-string #:read-file-forms)
   (:export #:run-gemini-conversation
